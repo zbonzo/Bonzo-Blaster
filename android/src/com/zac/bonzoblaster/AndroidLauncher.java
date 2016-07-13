0da508e135bfa17d8c03eaf2052d83b1d8800693
zbonzo <zbonzo@gmail.com>
2016-07-13 06:52:09 -0700

    first commit

---
+++ android/src/com/zac/bonzoblaster/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.zac.bonzoblaster;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.zac.bonzoblaster.BonzoBlaster;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BonzoBlaster(), config);
+	}
+}
